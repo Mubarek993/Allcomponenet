5ef25eca07cf3f9f5bd279e505280aa7242a4b9a
mubarkshegergmail@.com <mubareksheger@gmail.com>
2025-10-27 23:50:25 +0200

    Add Apple React Components project with Netlify deployment configuration

---
+++ my-app/src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/App.css";
+
+
+// import ('https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,600,600i,700,700i,800,800i,900,900i&display=swap');
+// import ('https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css');
+// import "font-awesome/css/font-awesome.min.css";
+
+
+
+
+
+
+
+import Header from '../../../all comopent/Header/Header.jsx'
+import Alart from '../../Alart/Alart.jsx';
+import Ipadpro from '../../Ipdpro/Ipadpro.jsx';
+import MAkBookair from '../../MAcBook Air/MAkBookair.jsx';
+import Iphone11pro from '../../iphon11pro/iphone11pro.jsx';
+import Iphone11 from '../../aiphoneandget the/iphone11.jsx';
+import AppleTv from '../../ppleTV/AppleTv.jsx';
+  import Applecared from '../../Applecared/Applecared.jsx';
+  import Footer from '../.././../all comopent/Footer/Footer.jsx'
+import AppleYOUtube from '../../AppleYOUTube/AppleYOUtube.jsx';
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+<>
+  <Header/>
+  <Alart/>
+  <Ipadpro/>
+  <MAkBookair/>
+  <Iphone11pro/>
+  <Iphone11/>
+  <AppleTv/>
+  <Applecared/>
+   <AppleYOUtube /> 
+  <Footer/> 
+</>
+  </StrictMode>
+)
